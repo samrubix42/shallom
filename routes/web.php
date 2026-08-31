bd9db36fbfa7c2da3c910b7077a46e7dd36e59c7
Saumy Anand <samcool3203@gmail.com>
2026-08-31 18:21:06 +0530

    route updated

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::livewire('/', 'pages::home')->name('home');
-Route::livewire('/products', 'pages::homelist')->name('products');
+Route::livewire('/our-range', 'pages::homelist')->name('products');
 Route::livewire('/profile', 'pages::profile')->name('profile');
 Route::livewire('/contact', 'pages::contact')->name('contact');
 
